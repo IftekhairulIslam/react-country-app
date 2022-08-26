224d149772b346020803fdc8487038041010fafe
Iftekhairul Islam <iftekhairul.islam@hotmail.com>
2022-08-27 00:21:37 +0600

    Reducer Refactoring

--- src/store/countryReducer.jsx
+++ src/store/countryReducer.jsx
@@ -5,42 +5,40 @@ export const INITIAL_COUNTRIES_STATE = {
 };
 
 /* ActionTypes */
-export const COUNTRIES_ACTION_TYPE = {
-  API_CALL_STARTED: "API_CALL_STARTED",
-  API_CALL_SUCCESS: "API_CALL_SUCCESS",
-  API_CALL_FAILED: "API_CALL_FAILED",
-};
+const API_CALL_STARTED = "API_CALL_STARTED";
+const API_CALL_SUCCESS = "API_CALL_SUCCESS";
+const API_CALL_FAILED = "API_CALL_FAILED";
 
 /* Actions / ActionCreators */
 export const startLoadingCountries = () => {
-  return { type: COUNTRIES_ACTION_TYPE.API_CALL_STARTED };
+  return { type: API_CALL_STARTED };
 };
 
 export const successtLoadingCountries = (countries) => {
   return {
-    type: COUNTRIES_ACTION_TYPE.API_CALL_SUCCESS,
+    type: API_CALL_SUCCESS,
     payload: countries,
   };
 };
 
 export const failedtLoadingCountries = (error) => {
-  return { type: COUNTRIES_ACTION_TYPE.API_CALL_FAILED, payload: error };
+  return { type: API_CALL_FAILED, payload: error };
 };
 
 /* Reducer */
 const countryReducer = (state, action) => {
   switch (action.type) {
-    case COUNTRIES_ACTION_TYPE.API_CALL_STARTED:
+    case API_CALL_STARTED:
       return {
         ...INITIAL_COUNTRIES_STATE,
         loading: true,
       };
-    case COUNTRIES_ACTION_TYPE.API_CALL_SUCCESS:
+    case API_CALL_SUCCESS:
       return {
         ...INITIAL_COUNTRIES_STATE,
         data: action.payload,
       };
-    case COUNTRIES_ACTION_TYPE.API_CALL_FAILED:
+    case API_CALL_FAILED:
       return {
         ...INITIAL_COUNTRIES_STATE,
         error: action.payload,
